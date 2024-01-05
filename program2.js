0ae4624807a42c61b6927d257432e0fe547ba994
Deepak Chouhan <96687987+deepakchouhandc@users.noreply.github.com>
2024-01-05 13:49:39 +0530

    Update program2.js
    Deepak Chouhan

--- program2.js
+++ program2.js
@@ -3,7 +3,29 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const map = new Map();
+    map.set('I', 1);
+    map.set('V', 5);
+    map.set('X', 10);
+    map.set('L', 50);
+    map.set('C', 100);
+    map.set('D', 500);
+    map.set('M', 1000);
+
+    // Assuming str is the Roman numeral string
+    const l = str.length;
+    let output = 0;
+
+    for (let i = 0; i < l; i++) {
+        if (i < l - 1 && map.get(str.charAt(i)) < map.get(str.charAt(i + 1))){
+            output += map.get(str.charAt(i + 1)) - map.get(str.charAt(i));
+            i++;
+        }
+        else {
+            output += map.get(str.charAt(i));
+        }
+    }
+    return output;
 };
 
 
