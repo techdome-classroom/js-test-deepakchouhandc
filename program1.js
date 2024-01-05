a8068594ccdbb7bd129ba80883c43ac0817f7338
Deepak Chouhan <96687987+deepakchouhandc@users.noreply.github.com>
2024-01-05 13:48:46 +0530

    Update program1.js
    deepak chouhan

--- program1.js
+++ program1.js
@@ -3,7 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+  if(s==null)
+  {
+      if (x == null){
+      return true;
+  }
+  let ch = x.split('');
+  let s = [];
+  for (let i = 0; i < ch.length; i++) {
+      if (ch[i] == '(' || ch[i] == '{' || ch[i] == '[') {
+          s.push(ch[i]);
+      } else if (ch[i] == ')' || ch[i] == '}' || ch[i] == ']') {
+          if (s.length === 0) {
+              return false;
+          }
+          let top = s.pop();
+          if ((ch[i] == ')' && top != '(') ||
+              (ch[i] == '}' && top != '{') ||
+              (ch[i] == ']' && top != '[')) {
+              return false;
+          }
+      }
+  }
+  return s.length === 0;  
 };
 
 module.exports = { isValid };
